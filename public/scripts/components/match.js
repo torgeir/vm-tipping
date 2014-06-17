0c815b9dcca3bb47b7b91f2cb2b857e7f00ecd6a
Jonas Follesø <jonas@follesoe.no>
2014-06-17 13:23:15 +0200

    Now marks all matches for a given user to check if they are correct or not.

--- public/scripts/components/match.js
+++ public/scripts/components/match.js
@@ -5,13 +5,25 @@ module.exports = React.createClass({
 
   render: function () {
     var match = this.match();
-    
+
+    var matchClass = 'no-outcome';
+
+    if (match.matchPlayed) {
+      if (match.correctResult) {
+        matchClass = 'correct-result';
+      } else if (match.correctOutcome) {
+        matchClass = 'correct-outcome';
+      } else {
+        matchClass = 'wrong-outcome';
+      }
+    }
+
     return (
-      <li className='bet'>
-        <span>{match.homename + ' - ' + match.awayname}</span> 
-        <span>{match.homegoals + ' - ' + match.awaygoals}</span>
+      <li className={matchClass + ' bet'}>
+        <span>{match.homename + ' - ' + match.awayname}</span>
+        <span>{match.homegoals + " - " + match.awaygoals}</span>
       </li>
-    );
+    );    
   },
 
   match: function () {
